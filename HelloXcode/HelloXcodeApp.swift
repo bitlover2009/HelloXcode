14bec3646d9b1bf8998efb0a0aedc44eedf0f9c4
bitlover2009 <bitlover2009@gmail.com>
2024-01-13 23:19:19 -0600

    Initial Commit

---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by David Grau Beltrán  on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
